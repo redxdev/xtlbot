5d9af7d4ea016b3e905b678548978473aa0778b4
Sam Bloomberg <sam@redxdev.com>
2015-06-28 15:24:27 -0700

    xtlbot won't start without config.lua or plugins.lua

--- xtlbot.lua
+++ xtlbot.lua
@@ -1,4 +1,29 @@
 -- xtlbot main
 -- call this file via "lua xtlbot.lua"
+local require = require
+local print = print
+
+local function file_exists(name)
+    local f=io.open(name,"r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
+end
+
+if not file_exists("config/config.lua") then
+    print "The main configuration file (config/config.lua) is missing! Run the configuration script."
+    print "Exiting!"
+    return
+end
+
+if not file_exists("config/plugins.lua") then
+    print "The plugin configuration file (config/plugins.lua) is missing! Run the configuration script."
+    print "Exiting!"
+    return
+end
+
 local core = require("src.core")
 core.init()
